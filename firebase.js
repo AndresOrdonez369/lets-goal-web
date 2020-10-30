6c0cf364a9b15bc274829528307aecd0d6882c37
Dylan <dachaconp@unal.edu.co>
2020-10-29 22:27:20 -0500

    firebase

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: 'AIzaSyDPLxKxN3DMOx8ngA3eMAdIX9QuoOHyAmw',
+  authDomain: 'klk-messenger.firebaseapp.com',
+  databaseURL: 'https://klk-messenger.firebaseio.com/',
+  projectId: 'klk-messenger',
+  storageBucket: 'klk-messenger.appspot.com',
+  messagingSenderId: '867379790697',
+  appId: '1:867379790697:web:b9936f99da8fce662ef3f8',
+  measurementId: 'G-PE3KSQLXP1',
+};
+firebase.initializeApp(config);
+
+export default firebase;
